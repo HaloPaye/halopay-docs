19b4d42a9209800ee2b33ea1902aafbb9f5ec92f
dillon ofili <dillonofili667@gmail.com>
2026-08-06 08:50:36 +0100

    feat(docs): initial scaffolding of Nextra documentation site

---
+++ src/pages/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  index: 'Introduction',
+  'protocol-mechanics': 'Protocol Mechanics',
+  'end-user-guides': 'End-User Guides',
+  'developer-guides': 'Developer Guides',
+  'technical-reference': 'Technical Reference',
+  'contributing': 'Contributing'
+}
---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <span>HaloPay Docs</span>,
+  project: {
+    link: 'https://github.com/HaloPaye',
+  },
+  docsRepositoryBase: 'https://github.com/HaloPaye/halopay-docs/tree/main',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – HaloPay'
+    }
+  },
+  footer: {
+    text: 'HaloPay - Stellar Drips Wave',
+  },
+}
