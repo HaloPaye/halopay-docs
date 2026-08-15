d6f29f641d40c31f6e72d64052d36583a65c6ffc
dillon ofili <dillonofili667@gmail.com>
2026-08-15 13:53:08 +0100

    docs: add logo as favicon

--- theme.config.jsx
+++ theme.config.jsx
@@ -4,6 +4,11 @@ export default {
     link: 'https://github.com/HaloPaye',
   },
   docsRepositoryBase: 'https://github.com/HaloPaye/halopay-docs/tree/main',
+  head: (
+    <>
+      <link rel="icon" type="image/png" href="/favicon.png" />
+    </>
+  ),
   useNextSeoProps() {
     return {
       titleTemplate: '%s – HaloPay'
